ea2584d3b2ace480b43bf0e15f7091684a25c6a1
Daniel18-dan <bernarddaniel171@gmail.com>
2026-01-05 05:42:53 +0000

    sg and subnets

--- env/dev/resource.tf
+++ env/dev/resource.tf
@@ -1,3 +1,27 @@
+resource "aws_security_group" "ec2_sg" {
+  name   = "ec2-sg-dev"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Environment = var.environment
+    Owner       = var.owner
+  }
+}
+
 module "ec2" {
   source        = "../../modules/ec2"
   ami_id        = var.ami_id
